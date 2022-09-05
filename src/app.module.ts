a3b1953db91e6c864d797ef9ee7f32f090fa9ee6
willian brandão mendonça <willianbr.ac68@gmail.com>
2022-09-05 07:55:01 -0500

    alter app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import AuthModule from './modules/auth.module';
+import graphqlConfig from './configs/graphql.config';
+import { AccountModule } from './modules/account.module';
+import { BookModule } from './modules/book.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    AccountModule,
+    BookModule,
+    GraphQLModule.forRoot(graphqlConfig),
+    TypeOrmModule.forRoot(),
+  ],
 })
 export class AppModule {}
